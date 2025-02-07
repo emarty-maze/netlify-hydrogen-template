ff9f5c0f335948d71e01c89b5dad2cfd0907a1af
Erik Marty <erik@themazegroup.com>
2025-02-07 09:36:15 -0600

    Testing builder integration

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,11 +1,40 @@
+import {
+  Content,
+  fetchOneEntry,
+  getBuilderSearchParams,
+  isPreviewing,
+} from '@builder.io/sdk-react';
 import type {LoaderFunctionArgs} from '@netlify/remix-runtime';
+import {useLoaderData} from '@remix-run/react';
+import {fetch as webFetch} from '@remix-run/web-fetch';
 
-export async function loader({request}: LoaderFunctionArgs) {
-  throw new Response(`${new URL(request.url).pathname} not found`, {
-    status: 404,
+const webFetch = fetch;
+const apiKey = '810e5adb1ee04fe3b583ce53929b2942';
+
+export const loader = async ({params, request}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const urlPath = `/${params['slug'] || ''}`;
+
+  const page = await fetchOneEntry({
+    model: 'page',
+    apiKey,
+    options: getBuilderSearchParams(url.searchParams),
+    userAttributes: {urlPath},
+    fetch: webFetch,
   });
-}
 
-export default function CatchAllPage() {
-  return null;
+  if (!page && !isPreviewing(url.search)) {
+    throw new Response('Page Not Found', {
+      status: 404,
+      statusText: 'Page not found in Builder.io',
+    });
+  }
+
+  return {page};
+};
+
+export default function Page() {
+  const {page} = useLoaderData<typeof loader>();
+
+  return <Content model="page" apiKey={apiKey} content={page as any} />;
 }
