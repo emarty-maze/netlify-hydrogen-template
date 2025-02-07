879e3cec940bc2cbb10a91c31f9dae15f63d7d8d
Erik Marty <erik@themazegroup.com>
2025-02-07 10:06:07 -0600

    Working with the builder docs to get preview working

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -6,35 +6,67 @@ import {
 } from '@builder.io/sdk-react';
 import type {LoaderFunctionArgs} from '@netlify/remix-runtime';
 import {useLoaderData} from '@remix-run/react';
-import {fetch as webFetch} from '@remix-run/web-fetch';
+// import {fetch as webFetch} from '@remix-run/web-fetch';
+import {BuilderComponent, useIsPreviewing} from '@builder.io/react';
+import builderPkg from '@builder.io/react';
+import type {BuilderContent} from '@builder.io/sdk';
 
 const webFetch = fetch;
 const apiKey = '810e5adb1ee04fe3b583ce53929b2942';
+const {builder} = builderPkg;
+builder.init(apiKey);
+builder.apiVersion = 'v3';
 
 export const loader = async ({params, request}: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const urlPath = `/${params['slug'] || ''}`;
 
-  const page = await fetchOneEntry({
-    model: 'page',
-    apiKey,
-    options: getBuilderSearchParams(url.searchParams),
-    userAttributes: {urlPath},
-    fetch: webFetch,
-  });
+  // const page = await fetchOneEntry({
+  //   model: 'page',
+  //   apiKey,
+  //   options: getBuilderSearchParams(url.searchParams),
+  //   userAttributes: {urlPath},
+  //   fetch: webFetch,
+  // });
 
-  if (!page && !isPreviewing(url.search)) {
-    throw new Response('Page Not Found', {
-      status: 404,
-      statusText: 'Page not found in Builder.io',
-    });
-  }
+  // if (!page && !isPreviewing(url.search)) {
+  //   throw new Response('Page Not Found', {
+  //     status: 404,
+  //     statusText: 'Page not found in Builder.io',
+  //   });
+  // }
 
-  return {page};
+  // return {page};
+
+  const page = await builder
+    .get('hydrogen-page-test', {
+      userAttributes: {
+        urlPath: '/' + params['*'],
+      },
+    })
+    .toPromise();
+
+  if (page) return page;
+
+  return {};
 };
 
-export default function Page() {
-  const {page} = useLoaderData<typeof loader>();
+// export default function Page() {
+//   const {page} = useLoaderData<typeof loader>();
+//
+//   return <Content model="page" apiKey={apiKey} content={page as any} />;
+// }
 
-  return <Content model="page" apiKey={apiKey} content={page as any} />;
+type BuilderContentRemix = Omit<BuilderContent, 'variations' | 'data'>;
+
+export default function Page() {
+  const page: BuilderContentRemix = useLoaderData<BuilderContentRemix>();
+  const isPreviewingInBuilder = useIsPreviewing();
+  const show404 = !page && !isPreviewingInBuilder;
+
+  if (show404) {
+    return <h1>404 not found (customize your 404 here)</h1>;
+  }
+
+  return <BuilderComponent model="hydrogen-page-test" content={page} />;
 }
